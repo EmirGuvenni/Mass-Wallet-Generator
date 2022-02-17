6620be9136f2dc0586e78e97abb5570b85498d7f
Emir Güvenni <contact@emirguvenni.com>
2022-02-17 13:10:26 +0300

    Added the script

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import Web3 from 'web3';
+import prompts, { PromptObject } from 'prompts';
+
+const questions: PromptObject[] = [
+  {
+    type: 'number',
+    name: 'numberOfWallets',
+    message: 'How many wallets do you want to generate?',
+    initial: 100,
+    min: 1,
+  },
+  {
+    type: 'text',
+    name: 'fileName',
+    message: 'Enter the output file name',
+    initial: 'wallets',
+    validate: (value: string) =>
+      value.length > 0 || 'Please enter a valid file name',
+  },
+];
+
+(async () => {
+  console.clear();
+
+  const params = await prompts(questions);
+
+  const web3 = new Web3('https://mainnet.infura.io/');
+
+  for (let i = 0; i < params.numberOfWallets; i++) {
+    const wallet = web3.eth.accounts.create(web3.utils.randomHex(32));
+
+    fs.appendFileSync(
+      `${params.fileName}.txt`,
+      `${wallet.address}=${wallet.privateKey}\n`
+    );
+  }
+})();
